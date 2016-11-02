9bda9412a64f8e5ca87bdd89e1b35b3b369d00cb
Nicolas Ameghino <nicolasam@vsisoft.com>
2016-11-02 16:59:27 -0300

    UIColor extension to specify color palette

---
+++ Upcoming/Classes/UIColor_Upcoming.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor_Upcoming.swift
+//  Upcoming
+//
+//  Created by Nico Ameghino on 11/2/16.
+//  Copyright © 2016 BlueTrail Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import HexString
+
+extension UIColor {
+    static let upcomingDarkGray = UIColor(hexString: "#333333")
+    static let upcomingYellow = UIColor(hexString: "#EDAD17")
+    static let upcomingLavender = UIColor(hexString: "#666A86")
+    static let upcomingLightBlue = UIColor(hexString: "#95B8D1")
+    static let upcomingSteelGray = UIColor(hexString: "#4A5043")
+}
